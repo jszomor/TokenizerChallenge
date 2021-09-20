525f49fe49a3728bbf41eda8c92a52862cd880ec
Szomor János <jszomor@gmail.com>
2021-09-20 11:41:22 +0200

    Main program added

---
+++ MainProgram/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using TokenizerLibrary;
+
+namespace MainProgram
+{
+  class Program
+  {
+    public static void Main()
+    {
+      string input = "almaKörteBarack";
+
+      foreach (var item in Tokenizer.Tokenize(input))
+      {
+        Console.WriteLine(item);
+      }
+    }
+  }
+}
+
